fdf204a96df21a6e046b0a90a071c2b17df5d54b
aloulou1238 <benhibaali@gmail.com>
2023-06-07 18:08:37 +0200

    update

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ app.get('/api/notes/:id', (request, response, next) => {
   .catch(error => next(error))
 })
 
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
 
   if (body.content === undefined) {
@@ -42,9 +42,11 @@ app.post('/api/notes', (request, response) => {
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
+  note.save()
+    .then(savedNote => {
     response.json(savedNote)
-  })
+    })
+    .catch(error => next(error))
 })
 
 app.delete('/api/notes/:id', (request, response, next) => {
@@ -56,14 +58,9 @@ app.delete('/api/notes/:id', (request, response, next) => {
 })
 
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const {content, important} = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(request.params.id, {content, important}, { new: true, runValidators: true, context: "query" })
     .then(updatedNote => {
       response.json(updatedNote)
     })
@@ -75,6 +72,8 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
+  }else if (error.name === 'ValidationError'){
+    return response.status(400).json({error: error.message})
   } 
 
   next(error)
